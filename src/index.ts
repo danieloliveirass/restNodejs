fd40955191e8f481726ad8dd9dddfbcb25cfa23c
danieloliveirass <dansilva.exe@gmail.com>
2022-07-19 20:00:21 -0300

    adição da authenticação basic para gerar jwt de login

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import errorHandler from './middlewares/error-handler.middleware';
+import authorizationRoute from './routes/authorization.route';
 import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
@@ -11,12 +13,11 @@ app.use(express.urlencoded({extended: true}));
 //configuração de Rotas
 app.use(statusRoute);
 app.use(usersRoute);
+app.use(authorizationRoute);
 
-/*
-app.get('/status', (req: Request,res: Response,next: NextFunction) => {
-    res.status(200).send({ foo: 'teste' });
-}); //rota simples com calback na function 
-*/
+
+// Configuração de Handlers de Erro
+app.use(errorHandler);
 
 //Inicialização do servidor
 app.listen(3000, () => {
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,33 +1,48 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import StatusCode from 'http-status-codes';
+import { DatabaseError } from 'pg';
+import { nextTick } from 'process';
+import userRepositoy from '../repositories/user.repositoy';
 
 //configuração de rotas
 const usersRoute = Router();
 
-usersRoute.get('/users', (req:Request, res: Response, next: NextFunction) => {
-    const users = [{ userName: 'Daniel'}];
+usersRoute.get('/users', async (req:Request, res: Response, next: NextFunction) => {
+    const users = await userRepositoy.findAllUsers();
     res.status(StatusCode.OK).send(users);
 });
 
-usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, Next: NextFunction) => {
-    const uuid = req.params.uuid;
-    res.status(StatusCode.OK).send({uuid});
+usersRoute.get('/users/:uuid', async (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+    try {
+        
+        const uuid = req.params.uuid;
+        const user = await userRepositoy.findById(uuid);
+        res.status(StatusCode.OK).send(user);
+    } catch(error){
+        next(error);
+    }
 });
 
-usersRoute.post('/users', (req: Request, res: Response, Next: NextFunction) => {
+usersRoute.post('/users', async (req: Request, res: Response, Next: NextFunction) => {
     const newUser = req.body;
 
-    console.log(req.body);
+    const uuid = await userRepositoy.create(newUser);
 
-    res.status(StatusCode.CREATED).send(newUser);
+    res.status(StatusCode.CREATED).send(uuid);
 });
 
-usersRoute.put('/users/:uuid', (req: Request<{uuid: string}>, res:Response, next:NextFunction) =>{
+usersRoute.put('/users/:uuid', async (req: Request<{uuid: string}>, res:Response, next:NextFunction) =>{
     const uuid = req.params.uuid;
-    res.status(StatusCode.OK).send({uuid});
+    const modifiedUser = req.body;
+    
+    await userRepositoy.update(modifiedUser);
+
+    res.status(StatusCode.OK).send();
 })
 
-usersRoute.delete('/users/:uuid', (req: Request<{uuid: string}>, res:Response, next:NextFunction) => {
+usersRoute.delete('/users/:uuid', async (req: Request<{uuid: string}>, res:Response, next:NextFunction) => {
+    const uuid = req.params.uuid;
+    await userRepositoy.remove(uuid);
     res.sendStatus(StatusCode.OK);
 });
 
